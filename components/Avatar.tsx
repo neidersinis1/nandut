143d2cd48253475b5dea0a8a1d17c2ee9227498e
Neider Sinisterra <neidersinis@hotmail.com>
2023-07-12 11:37:44 -0500

    Ready

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -45,9 +45,12 @@ const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
         alt="Avatar"
         onClick={onClick}
         src={fetchedUser?.profileImage || '/images/placeholder.png'}
+        // width={60}
+        // height={60}
+        sizes="60"
       />
     </div>
   );
 }
- 
+
 export default Avatar;
